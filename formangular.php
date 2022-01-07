612a1839c1a772b2097ee67425872c7fc9e81e81
Arun Singh Rathore <97164053+Arun10008@users.noreply.github.com>
2022-01-07 15:36:59 +0530

    Update formangular.php

--- formangular.php
+++ formangular.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
  
-$email_from = 'Website Design.com';
+$email_from = 'gs213710@gmail.com';
 
 $email_subject = 'New Form Submission';
 
@@ -13,7 +13,7 @@ $email_body = "User Name: $name.\n".
                "Subject: $subject.\n".
                 "User Message: $message .\n";
 
-$to = 'gs213710@gamil.com';
+$to = 'ar9462839@gmail.com';
 
 $headers = "from: $email_from \r\n";
 
